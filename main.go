0339ea60d99e67a4bf5c70ae370fac8e454d9cb8
neoCipher (Kaleab Alemayehu) <cipherlofi04@gmail.com>
2024-12-03 07:57:57 -0500

    solving day2 challenge 1

--- main.go
+++ main.go
@@ -1,47 +1,84 @@
 package main
 
 import (
+	// "fmt"
+	// "github.com/kaleabAlemayehu/Aoc2024/lexer"
 	"fmt"
-	"github.com/kaleabAlemayehu/Aoc2024/lexer"
 	"os"
+	"strconv"
 )
 
 func main() {
 	// Reading file
-	data, err := os.ReadFile("./input.txt")
+	// data, err := os.ReadFile("./testd2.txt")
+	data, err := os.ReadFile("./inputd2.txt")
 	if err != nil {
 		panic(err)
 	}
-	// Parse file input and store them into their two array
-	var left = []int{}
-	var right = []int{}
-	l := lexer.NewLexer(data)
-	for i := 0; i < 2000; i++ {
-		v := l.NextToken()
-		if i%2 == 0 {
-			left = append(left, int(v))
-		} else {
-			right = append(right, int(v))
-		}
+	// Create super 2d array
+	store := make([][]int, 1000)
+	for i := range store {
+		store[i] = make([]int, 8)
 	}
-	// Create fq counter the right list
-	fq := make(map[int]int)
-	for _, v := range right {
-		i, ok := fq[v]
-		if !ok {
-			fq[v] = 1
-		} else {
-			fq[v] = i + 1
-		}
-	}
-	var sum int
-	// Loop over element of left and sum the value * frequency_of_value
-	for _, v := range left {
-		i, ok := fq[v]
-		if ok {
-			sum += v * i
-		}
-	}
-	fmt.Println("the sum is:", sum)
+	var i, j int
+	for index := 0; index < len(data); {
+		if data[index] == 10 {
+			j++
+			index++
+			// New sub-array
+			i = 0
 
+		} else if data[index] == 32 {
+			i++
+			index++
+			// Add new element in sub-array
+		} else {
+			// Read number
+			val := readNumber(data, &index)
+			store[j][i], err = strconv.Atoi(string(val))
+			if err != nil {
+				panic(err)
+			}
+		}
+
+	}
+	var totalSafe int
+	for _, value := range store {
+		// Check if it is safe
+		totalSafe = totalSafe + CheckSafe(value)
+	}
+	fmt.Printf("totalSafe:%d\n", totalSafe)
+}
+
+func CheckSafe(arr []int) int {
+	// Determine if it incremental or not
+	var isInc bool
+	if arr[0] > arr[1] && (arr[0]-arr[1] <= 3 && arr[0]-arr[1] >= 1) {
+		isInc = false
+	} else if arr[0] < arr[1] && (arr[1]-arr[0] <= 3 && arr[1]-arr[0] >= 1) {
+		isInc = true
+	} else {
+		return 0
+	}
+	for i := 2; i < len(arr); i++ {
+		if ((arr[i] > arr[i-1] && isInc) && (arr[i]-arr[i-1] >= 1 && arr[i]-arr[i-1] <= 3)) || arr[i] == 0 {
+
+		} else if ((arr[i] < arr[i-1] && !isInc) && (arr[i-1]-arr[i] >= 1 && arr[i-1]-arr[i] <= 3)) || arr[0] == 0 {
+		} else {
+			return 0
+		}
+	}
+	return 1
+}
+
+func readNumber(data []byte, i *int) []byte {
+	position := *i
+	for isDigit(data[*i]) {
+		*i++
+	}
+	return data[position:*i]
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
